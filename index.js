0a92ec376050f3a7db584d43baa3d91dba5c5fad
Robert <bates.to.infinity@gmail.com>
2021-10-26 21:44:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ['Lisa', 'Kaitlin', 'Jan']
+const thankYou = []
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        thankYou.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYou 
+}
+
+function countDown(number) {
+    let down = number;
+    while (down >= 0) {
+        console.log(down--)
+    }
+}
